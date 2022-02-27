7e753989960c4e8a54172340fd380369ad51d826
Rek-Malorm <70321198+Rek-Malorm@users.noreply.github.com>
2022-02-27 20:36:57 +1300

    some bytes

---
+++ rustaid-number/src/byte.rs
@@ -0,0 +1,32 @@
+use crate::number::some_number;
+use crate::numbers::some_number;
+
+pub fn some_byte() -> u8 {
+    some_number()
+}
+
+pub fn some_byte_vector(bound: usize) -> Vec<u8> {
+    let mut vec = Vec::new();
+    for _ in 0..bound {
+        vec.push(some_number())
+    }
+    vec
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::number::*;
+    use super::*;
+
+    #[test]
+    fn can_create_random_byte() {
+        let _byte = some_byte();
+    }
+
+    #[test]
+    fn can_create_random_byte_vector() {
+        let bound = some_number_between(1, 1024);
+        let byte_vector = some_byte_vector(bound);
+        assert_eq!(byte_vector.len(), bound);
+    }
+}
