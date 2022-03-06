bc7983180afa081813082068ca8c820813c8477d
Rek-Malorm <70321198+Rek-Malorm@users.noreply.github.com>
2022-03-06 19:02:39 +1300

    Bug fix and documentation for string

--- src/string.rs
+++ src/string.rs
@@ -1,68 +1,272 @@
 use crate::number::{some_number_between, some_number_less_than};
 
 const ALPHA_CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
-                            abcdefghijklmnopqrstuvwxyz";
+                            abcdefghijklmnopqrstuvwxyz ";
 const NUMERIC_CHARSET: &[u8] = b"0123456789";
 
 const ALPHANUMERIC_CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
-                            0123456789";
+                            0123456789 ";
 
 const ASCII_CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
                             0123456789!@#$%^&*(){}[]\"\
-                            <>',.?+|_/=\\-:;`~";
+                            <>',.?+|_/=\\-:;`~ ";
 
 const DEFAULT_MIN_LENGTH: usize = 1;
 const DEFAULT_MAX_LENGTH: usize = 1024;
 
+/// Creates a new string of random characters
+///
+/// Creates a new string of a random length between 1 and 1024 characters.
+/// Each character can be any ASCII value with a valid character value (Excluding values such as
+/// NUL, BELL, DEL) and does not include the extended ASCII character set.
+/// Complete set is defined as a space and the following
+/// > ABCDEFGHIJKLMNOPQRSTUVWXYZ
+/// >
+/// > abcdefghijklmnopqrstuvwxyz
+/// >
+/// > 0123456789!@#$%^&*(){}[]\"
+/// >
+/// > <>',.?+|_/=\\-:;`~
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// let s = string::some_string();
+/// ```
 pub fn some_string() -> String {
-    some_string_of_length(DEFAULT_MAX_LENGTH)
+    some_string_of_length_between(DEFAULT_MIN_LENGTH,DEFAULT_MAX_LENGTH)
 }
 
+/// Creates a new string of random characters for a set length
+///
+/// Creates a new string of `to_bound: usize` characters.
+/// Each character can be any ASCII value with a valid character value (Excluding values such as
+/// NUL, BELL, DEL) and does not include the extended ASCII character set.
+/// Complete set is defined as a space and the following
+/// > ABCDEFGHIJKLMNOPQRSTUVWXYZ
+/// >
+/// > abcdefghijklmnopqrstuvwxyz
+/// >
+/// > 0123456789!@#$%^&*(){}[]\"
+/// >
+/// > <>',.?+|_/=\\-:;`~
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// let s = string::some_string_of_length(50);
+/// ```
 pub fn some_string_of_length(to_bound: usize) -> String {
-    some_string_of_length_between(DEFAULT_MIN_LENGTH, to_bound)
+    some_string_of_length_between(to_bound, to_bound)
 }
 
+/// Creates a new string of random characters for a length between two values
+///
+/// Creates a new string of random characters between `from_bound: usize` and `to_bound: usize`
+/// Each character can be any ASCII value with a valid character value (Excluding values such as
+/// NUL, BELL, DEL) and does not include the extended ASCII character set.
+/// Complete set is defined as a space and the following
+/// > ABCDEFGHIJKLMNOPQRSTUVWXYZ
+/// >
+/// > abcdefghijklmnopqrstuvwxyz
+/// >
+/// > 0123456789!@#$%^&*(){}[]\"
+/// >
+/// > <>',.?+|_/=\\-:;`~
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// let s = string::some_string_of_length_between(50, 100);
+/// ```
 pub fn some_string_of_length_between(from_bound: usize, to_bound: usize) -> String {
     let bound = some_number_between(from_bound, to_bound);
     string_for_charset(bound, ASCII_CHARSET)
 }
 
+/// Creates a new string of random alphanumeric characters
+///
+/// Creates a new string of a random length between 1 and 1024 alphanumeric characters.
+/// Each character can be any ASCII value with a valid character value (Excluding values such as
+/// NUL, BELL, DEL) and does not include the extended ASCII character set.
+/// Complete set is defined as a space and the following
+/// > ABCDEFGHIJKLMNOPQRSTUVWXYZ
+/// >
+/// > abcdefghijklmnopqrstuvwxyz
+/// >
+/// > 0123456789
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// let s = string::some_alphanumeric_string();
+/// ```
 pub fn some_alphanumeric_string() -> String {
-    some_alphanumeric_string_of_length(DEFAULT_MAX_LENGTH)
+    some_alphanumeric_string_of_length_between(DEFAULT_MIN_LENGTH, DEFAULT_MAX_LENGTH)
 }
 
+/// Creates a new string of random alphanumeric characters for a set length
+///
+/// Creates a new string of `to_bound: usize` alphanumeric characters.
+/// Each character can be any ASCII value with a valid character value (Excluding values such as
+/// NUL, BELL, DEL) and does not include the extended ASCII character set.
+/// Complete set is defined as a space and the following
+/// > ABCDEFGHIJKLMNOPQRSTUVWXYZ
+/// >
+/// > abcdefghijklmnopqrstuvwxyz
+/// >
+/// > 0123456789
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// let s = string::some_alphanumeric_string_of_length(50);
+/// ```
 pub fn some_alphanumeric_string_of_length(to_bound: usize) -> String {
-    some_alphanumeric_string_of_length_between(DEFAULT_MIN_LENGTH, to_bound)
+    some_alphanumeric_string_of_length_between(to_bound, to_bound)
 }
 
+/// Creates a new string of random alphanumeric characters for a length between two values
+///
+/// Creates a new string of random alphanumeric characters between `from_bound: usize` and `to_bound: usize`
+/// Each character can be any ASCII value with a valid character value (Excluding values such as
+/// NUL, BELL, DEL) and does not include the extended ASCII character set.
+/// Complete set is defined as a space and the following
+/// > ABCDEFGHIJKLMNOPQRSTUVWXYZ
+/// >
+/// > abcdefghijklmnopqrstuvwxyz
+/// >
+/// > 0123456789
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// let s = string::some_alpha_string_of_length_between(50, 100);
+/// ```
 pub fn some_alphanumeric_string_of_length_between(from_bound: usize, to_bound: usize) -> String {
     let bound = some_number_between(from_bound, to_bound);
     string_for_charset(bound, ALPHANUMERIC_CHARSET)
 }
 
+/// Creates a new string of random numeric characters
+///
+/// Creates a new string of a random length between 1 and 1024 numeric characters.
+/// Each character can be any ASCII value with a valid character value (Excluding values such as
+/// NUL, BELL, DEL) and does not include the extended ASCII character set.
+/// Complete set is defined as the following
+/// > 0123456789
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// let s = string::some_numeric_string();
+/// ```
 pub fn some_numeric_string() -> String {
-    some_numeric_string_of_length( DEFAULT_MAX_LENGTH)
+    some_numeric_string_of_length_between( DEFAULT_MIN_LENGTH, DEFAULT_MAX_LENGTH)
 }
 
+/// Creates a new string of random numeric characters for a set length
+///
+/// Creates a new string of `to_bound: usize` numeric characters.
+/// Each character can be any ASCII value with a valid character value (Excluding values such as
+/// NUL, BELL, DEL) and does not include the extended ASCII character set.
+/// Complete set is defined as the following
+/// > 0123456789
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// let s = string::some_numeric_string_of_length(50);
+/// ```
 pub fn some_numeric_string_of_length(to_bound: usize) -> String {
-    some_numeric_string_of_length_between(DEFAULT_MIN_LENGTH, to_bound)
+    some_numeric_string_of_length_between(to_bound, to_bound)
 }
 
+/// Creates a new string of random numeric characters for a length between two values
+///
+/// Creates a new string of random numeric characters between `from_bound: usize` and `to_bound: usize`
+/// Each character can be any ASCII value with a valid character value (Excluding values such as
+/// NUL, BELL, DEL) and does not include the extended ASCII character set.
+/// Complete set is defined as the following
+/// > 0123456789
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// let s = string::some_numeric_string_of_length_between(50, 100);
+/// ```
 pub fn some_numeric_string_of_length_between(from_bound: usize, to_bound: usize) -> String {
     let bound = some_number_between(from_bound, to_bound);
     string_for_charset(bound, NUMERIC_CHARSET)
 }
 
+/// Creates a new string of random alpha characters
+///
+/// Creates a new string of a random length between 1 and 1024 alpha characters.
+/// Each character can be any ASCII value with a valid character value (Excluding values such as
+/// NUL, BELL, DEL) and does not include the extended ASCII character set.
+/// Complete set is defined as the following
+/// > ABCDEFGHIJKLMNOPQRSTUVWXYZ
+/// >
+/// > abcdefghijklmnopqrstuvwxyz
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// let s = string::some_numeric_string();
+/// ```
 pub fn some_alpha_string() -> String {
-    some_alpha_string_of_length(DEFAULT_MAX_LENGTH)
+    some_alpha_string_of_length_between(DEFAULT_MIN_LENGTH, DEFAULT_MAX_LENGTH)
 }
 
+/// Creates a new string of random alpha characters for a set length
+///
+/// Creates a new string of `to_bound: usize` alpha characters.
+/// Each character can be any ASCII value with a valid character value (Excluding values such as
+/// NUL, BELL, DEL) and does not include the extended ASCII character set.
+/// Complete set is defined as the following
+/// > ABCDEFGHIJKLMNOPQRSTUVWXYZ
+/// >
+/// > abcdefghijklmnopqrstuvwxyz
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// let s = string::some_alpha_string_of_length(50);
+/// ```
 pub fn some_alpha_string_of_length(to_bound: usize) -> String {
-    some_alpha_string_of_length_between(DEFAULT_MIN_LENGTH, to_bound)
+    some_alpha_string_of_length_between(to_bound, to_bound)
 }
 
+/// Creates a new string of random alpha characters for a length between two values
+///
+/// Creates a new string of random alpha characters between `from_bound: usize` and `to_bound: usize`
+/// Each character can be any ASCII value with a valid character value (Excluding values such as
+/// NUL, BELL, DEL) and does not include the extended ASCII character set.
+/// Complete set is defined as the following
+/// > ABCDEFGHIJKLMNOPQRSTUVWXYZ
+/// >
+/// > abcdefghijklmnopqrstuvwxyz
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// let s = string::some_alpha_string_of_length_between(50, 100);
+/// ```
 pub fn some_alpha_string_of_length_between(from_bound: usize, to_bound: usize) -> String {
     let bound = some_number_between(from_bound, to_bound);
     string_for_charset(bound, ALPHA_CHARSET)
